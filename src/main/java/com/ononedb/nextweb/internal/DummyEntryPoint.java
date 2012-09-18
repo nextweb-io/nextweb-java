dd848516180b1895a26cdf59acefd7cfd3922843
commit <commit@noemail.com>
2012-09-19 10:50:39 +1200

    first link with JS engine successful

--- src/main/java/com/ononedb/nextweb/internal/DummyEntryPoint.java
+++ src/main/java/com/ononedb/nextweb/internal/DummyEntryPoint.java
@@ -2,8 +2,12 @@ package com.ononedb.nextweb.internal;
 
 import org.timepedia.exporter.client.ExporterUtil;
 
+import com.ononedb.nextweb.js.OnedbNextwebJsEngine;
+
 public class DummyEntryPoint {
 	public void onModuleLoad() {
 		ExporterUtil.exportAll();
+
+		OnedbNextwebJsEngine.init();
 	}
 }
--- src/main/java/io/nextweb/internal/DummyEntryPoint.java
+++ src/main/java/io/nextweb/internal/DummyEntryPoint.java
@@ -1,9 +1,8 @@
 package io.nextweb.internal;
 
-import org.timepedia.exporter.client.ExporterUtil;
 
 public class DummyEntryPoint {
 	public void onModuleLoad() {
-		ExporterUtil.exportAll();
+		// ExporterUtil.exportAll();
 	}
 }
