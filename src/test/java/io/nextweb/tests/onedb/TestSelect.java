4101fba37ea0ecb28e041175fcfa47c32a981980
commit <commit@noemail.com>
2012-09-28 19:34:43 +1200

    mutlithreading in append case now works

--- src/test/java/io/nextweb/tests/onedb/TestSelect.java
+++ src/test/java/io/nextweb/tests/onedb/TestSelect.java
@@ -8,8 +8,8 @@ import io.nextweb.Query;
 import io.nextweb.Session;
 import io.nextweb.engine.NextwebEngine;
 import io.nextweb.fn.Closure;
-import junit.framework.Assert;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import com.ononedb.nextweb.jre.OnedbNextwebJreEngine;
@@ -19,19 +19,20 @@ public class TestSelect {
 	@Test
 	public void testSelect() {
 
-		NextwebEngine engine = OnedbNextwebJreEngine.init();
+		final NextwebEngine engine = OnedbNextwebJreEngine.init();
 
-		Session session = engine.createSession();
+		final Session session = engine.createSession();
 
-		Link questionBagRepository = session
+		final Link questionBagRepository = session
 				.node("http://slicnet.com/seed1/seed1/9/1/h/sd");
 
-		Link aQuestionBag = session
+		final Link aQuestionBag = session
 				.node("http://slicnet.com/mxrogm/mxrogm/apps/nodejump/docs/8/n/Types/Question_Bag");
 
-		Query questionBagQuery = questionBagRepository.select(aQuestionBag);
+		final Query questionBagQuery = questionBagRepository
+				.select(aQuestionBag);
 
-		Node questionBag = questionBagQuery.get();
+		final Node questionBag = questionBagQuery.get();
 
 		Assert.assertTrue(questionBag.getValue() != null);
 
@@ -42,21 +43,22 @@ public class TestSelect {
 	@Test
 	public void testParallelSelect() {
 
-		NextwebEngine engine = OnedbNextwebJreEngine.init();
+		final NextwebEngine engine = OnedbNextwebJreEngine.init();
 
-		Session session = engine.createSession();
+		final Session session = engine.createSession();
 
-		Link questionBagRepository = session
+		final Link questionBagRepository = session
 				.node("http://slicnet.com/seed1/seed1/9/1/h/sd");
 
-		Link aQuestionBag = session
+		final Link aQuestionBag = session
 				.node("http://slicnet.com/mxrogm/mxrogm/apps/nodejump/docs/8/n/Types/Question_Bag");
 
-		Query questionBagQuery = questionBagRepository.select(aQuestionBag);
+		final Query questionBagQuery = questionBagRepository
+				.select(aQuestionBag);
 
 		session.getAll(questionBagRepository, aQuestionBag, questionBagQuery);
 
-		Node questionBag = questionBagQuery.get();
+		final Node questionBag = questionBagQuery.get();
 
 		Assert.assertTrue(questionBag.getValue() != null);
 
@@ -66,22 +68,25 @@ public class TestSelect {
 
 	@Test
 	public void testSelectPlugin() {
-		NextwebEngine engine = OnedbNextwebJreEngine.init();
+		final NextwebEngine engine = OnedbNextwebJreEngine.init();
 
-		Session session = engine.createSession();
+		final Session session = engine.createSession();
 
-		Link questionBagRepository = session
+		final Link questionBagRepository = session
 				.node("http://slicnet.com/seed1/seed1/9/1/h/sd");
 
-		Link aQuestionBag = session
+		final Link aQuestionBag = session
 				.node("http://slicnet.com/mxrogm/mxrogm/apps/nodejump/docs/8/n/Types/Question_Bag");
 
-		Query questionBagQuery = questionBagRepository.plugin(
-				engine.plugin().select()).select(aQuestionBag);
+		// Query questionBagQuery = questionBagRepository.plugin(
+		// engine.plugin().select()).select(aQuestionBag);
+
+		final Query questionBagQuery = questionBagRepository
+				.select(aQuestionBag);
 
 		session.getAll(questionBagRepository, aQuestionBag, questionBagQuery);
 
-		Node questionBag = questionBagQuery.get();
+		final Node questionBag = questionBagQuery.get();
 
 		Assert.assertTrue(questionBag.getValue() != null);
 
@@ -90,21 +95,21 @@ public class TestSelect {
 
 	@Test
 	public void testSelectAll() {
-		NextwebEngine engine = OnedbNextwebJreEngine.init();
+		final NextwebEngine engine = OnedbNextwebJreEngine.init();
 
-		Session session = engine.createSession();
+		final Session session = engine.createSession();
 
-		Link questions = session
+		final Link questions = session
 				.node("http://slicnet.com/seed1/seed1/9/1/h/sd/questions");
 
-		ListQuery allQuestionsQuery = questions.selectAll();
+		final ListQuery allQuestionsQuery = questions.selectAll();
 
-		NodeList allQuestions = allQuestionsQuery.get();
+		final NodeList allQuestions = allQuestionsQuery.get();
 
 		allQuestions.each(new Closure<Node>() {
 
 			@Override
-			public void apply(Node o) {
+			public void apply(final Node o) {
 
 			}
 		});
@@ -115,20 +120,20 @@ public class TestSelect {
 
 	@Test
 	public void testSelectNested() {
-		NextwebEngine engine = OnedbNextwebJreEngine.init();
+		final NextwebEngine engine = OnedbNextwebJreEngine.init();
 
-		Session session = engine.createSession();
+		final Session session = engine.createSession();
 
-		Link isQuestion = session
+		final Link isQuestion = session
 				.node("http://slicnet.com/mxrogm/mxrogm/apps/nodejump/docs/8/n/Types/Strategy_Quadrant_Questi");
 
-		Link isBrandName = session
+		final Link isBrandName = session
 				.node("http://slicnet.com/mxrogm/mxrogm/apps/nodejump/docs/8/n/Types/Brand_Name");
 
-		Link questions = session
+		final Link questions = session
 				.node("http://slicnet.com/seed1/seed1/9/1/h/sd/questions");
 
-		NodeList brandNames = questions.selectAll(isQuestion)
+		final NodeList brandNames = questions.selectAll(isQuestion)
 				.select(isBrandName).get();
 
 		System.out.println("Brand names: " + brandNames.values());
@@ -139,20 +144,20 @@ public class TestSelect {
 
 	@Test
 	public void testHas() {
-		NextwebEngine engine = OnedbNextwebJreEngine.init();
+		final NextwebEngine engine = OnedbNextwebJreEngine.init();
 
-		Session session = engine.createSession();
+		final Session session = engine.createSession();
 
-		Link isQuestion = session
+		final Link isQuestion = session
 				.node("http://slicnet.com/mxrogm/mxrogm/apps/nodejump/docs/8/n/Types/Strategy_Quadrant_Questi");
 
-		Link isBrandName = session
+		final Link isBrandName = session
 				.node("http://slicnet.com/mxrogm/mxrogm/apps/nodejump/docs/8/n/Types/Brand_Name");
 
-		Link isBrandImage = session
+		final Link isBrandImage = session
 				.node("http://slicnet.com/mxrogm/mxrogm/apps/nodejump/docs/8/n/Types/Brand_Image");
 
-		Link question = session
+		final Link question = session
 				.node("http://slicnet.com/seed1/seed1/7/0/h/sd/q");
 
 		Assert.assertTrue(question.has(isBrandName)
