2f2c73e6ed4d0c21b1855a310edf704b02828bba
commit <commit@noemail.com>
2012-10-08 15:35:11 +1300

    working on generating documentation with Maven

--- src/test/java/io/nextweb/tests/entity/TestAppend_WithAddress.java
+++ src/test/java/io/nextweb/tests/entity/TestAppend_WithAddress.java
@@ -1,5 +1,12 @@
 package io.nextweb.tests.entity;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestAppend_WithAddress {
 
+	@Test
+	public void testAppendToQuery() {
+		Assert.assertTrue(true);
+	}
 }
