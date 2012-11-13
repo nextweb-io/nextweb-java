00c8748873b101d224ae4e0460182532676efc74
mxro <mxro>
2012-11-13 13:58:10 +1300

    adding comoon node types to factory

--- src/main/java/io/nextweb/engine/Factory.java
+++ src/main/java/io/nextweb/engine/Factory.java
@@ -3,6 +3,9 @@ package io.nextweb.engine;
 import io.nextweb.Session;
 import io.nextweb.fn.AsyncResult;
 import io.nextweb.fn.Result;
+import io.nextweb.nodes.Bytes;
+import io.nextweb.nodes.Port;
+import io.nextweb.nodes.Token;
 import io.nextweb.operations.exceptions.ExceptionManager;
 
 public interface Factory {
@@ -10,5 +13,12 @@ public interface Factory {
 	public <ResultType> Result<ResultType> createResult(
 			ExceptionManager exceptionManager, Session session,
 			AsyncResult<ResultType> asyncResult);
+
+	public Bytes createBytes(byte[] data, String mimetype);
+
+	public Port createPort(String uri, String secret);
+
+	public Token createToken(String secret, String grantedAuthorization,
+			String identification);
 
 }
