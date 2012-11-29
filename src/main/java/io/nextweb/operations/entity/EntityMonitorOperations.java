bdb564011eeb68b5995b152818ba6f3c061aa367
mxro <mxro@nowhere.com>
2012-11-30 09:14:10 +1300

    implementing new monitor API

--- src/main/java/io/nextweb/operations/entity/EntityMonitorOperations.java
+++ src/main/java/io/nextweb/operations/entity/EntityMonitorOperations.java
@@ -1,13 +1,14 @@
 package io.nextweb.operations.entity;
 
-import io.nextweb.Node;
 import io.nextweb.common.Interval;
 import io.nextweb.common.Monitor;
+import io.nextweb.common.MonitorContext;
 import io.nextweb.fn.Closure;
 import io.nextweb.fn.Result;
 
 public interface EntityMonitorOperations {
 
-	public Result<Monitor> monitor(Interval interval, Closure<Node> whenChanged);
+	public Result<Monitor> monitor(Interval interval,
+			Closure<MonitorContext> whenChanged);
 
 }
