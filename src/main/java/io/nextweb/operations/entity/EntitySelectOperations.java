ba0c149c1d58b465e9647111bef0fbd03fbbfa1c
mxro <mxro@nowhere.com>
2013-01-14 14:56:53 +1300

    thought about introducing new variant selectIfExists - but this is not consitent with the general logic ... Maybe only if it is resolved to a Maybe<Node> ?!?!

--- src/main/java/io/nextweb/operations/entity/EntitySelectOperations.java
+++ src/main/java/io/nextweb/operations/entity/EntitySelectOperations.java
@@ -8,76 +8,76 @@ import io.nextweb.fn.BooleanResult;
 
 public interface EntitySelectOperations {
 
-	/**
-	 * <p>
-	 * Select the first child node with the specified property type.
-	 * </p>
-	 * <p>
-	 * For more information on this operation, please see the Nextweb
-	 * documentation: <a
-	 * href="http://nextweb.io/docs/nextweb-select-property.value.html">select
-	 * (property)</a>
-	 * </p>
-	 * 
-	 * @param propertyType
-	 * @return
-	 */
-	public Query select(Link propertyType);
+    /**
+     * <p>
+     * Select the first child node with the specified property type.
+     * </p>
+     * <p>
+     * For more information on this operation, please see the Nextweb
+     * documentation: <a
+     * href="http://nextweb.io/docs/nextweb-select-property.value.html">select
+     * (property)</a>
+     * </p>
+     * 
+     * @param propertyType
+     * @return
+     */
+    public Query select(Link propertyType);
 
-	/**
-	 * <p>
-	 * Tries to find a child with the given propertyType value.
-	 * </p>
-	 * <p>
-	 * If no child is available, a node will be appended with the given
-	 * defaultValue.
-	 * </p>
-	 * 
-	 * @param propertyType
-	 * @param defaultValue
-	 * @return
-	 */
-	public Query select(Link propertyType, Object defaultValue);
+    /**
+     * <p>
+     * Tries to find a child with the given propertyType value.
+     * </p>
+     * <p>
+     * If no child is available, a node will be appended with the given
+     * defaultValue.
+     * </p>
+     * 
+     * @param propertyType
+     * @param defaultValue
+     * @return
+     */
+    public Query select(Link propertyType, Object defaultValue);
 
-	/**
-	 * <p>
-	 * Query for a node at the specified path relative to this node.
-	 * </p>
-	 * 
-	 * @param path
-	 * @return
-	 */
-	public Query select(String path);
+    /**
+     * <p>
+     * Query for a node at the specified path relative to this node.
+     * </p>
+     * 
+     * @param path
+     * @return
+     */
+    public Query select(String path);
 
-	/**
-	 * <p>
-	 * Query for a node at the specified path.
-	 * </p>
-	 * <p>
-	 * If no node exists, insert a new node at the specified location with the
-	 * given defaultValue.
-	 * </p>
-	 * 
-	 * @param path
-	 * @param defaultValue
-	 * @return
-	 */
-	public Query select(String path, Object defaultValue);
+    /**
+     * <p>
+     * Query for a node at the specified path.
+     * </p>
+     * <p>
+     * If no node exists, insert a new node at the specified location with the
+     * given defaultValue.
+     * </p>
+     * 
+     * @param path
+     * @param defaultValue
+     * @return
+     */
+    public Query select(String path, Object defaultValue);
 
-	public ListQuery selectAll(Link propertyType);
+    public ListQuery selectAll(Link propertyType);
 
-	public LinkListQuery selectAllLinks();
+    public LinkListQuery selectAllLinks();
 
-	public ListQuery selectAll();
+    public ListQuery selectAll();
 
-	public BooleanResult has(Link propertyType);
+    public BooleanResult has(Link propertyType);
 
-	/**
-	 * Not supported yet.
-	 * 
-	 * @param propertyType
-	 * @return
-	 */
-	public Query ifHas(Link propertyType);
+    /**
+     * Not supported yet.
+     * 
+     * @param propertyType
+     * @return
+     */
+    public Query ifHas(Link propertyType);
 
 }
