dfaf02645f31bfd9009c3849e44018834a260d38
mxro <mxro@nowhere.com>
2013-11-14 06:23:16 +1300

    updating configuration

--- src/main/java/io/nextweb/common/SessionConfiguration.java
+++ src/main/java/io/nextweb/common/SessionConfiguration.java
@@ -4,14 +4,24 @@ import io.nextweb.engine.persistence.PersistenceConnection;
 
 public abstract class SessionConfiguration {
 
-	
+	public static SessionConfiguration withPersistedSession(
+			final PersistenceConnection connection) {
+		return new SessionConfiguration() {
+
+			@Override
+			public PersistenceConnection connectionToPersistedSessionNetwork() {
+				return connection;
+			}
+
+		};
+	}
+
 	public PersistenceConnection connectionToPersistedSessionNetwork() {
 		return null;
 	}
-	
+
 	public PersistenceConnection connectionToPersistedCache() {
 		return null;
 	}
-	
-	
+
 }
