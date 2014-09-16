6e50f25fdcc2d5002509fcd66f673dd34a038cde
mxro <mxro@nowhere.com>
2014-09-17 08:35:03 +1000

    Updating documentation Autocommit Wed Sep 17 08:35:03 EST 2014

--- src/main/java/io/nextweb/Query.java
+++ src/main/java/io/nextweb/Query.java
@@ -2,6 +2,12 @@ package io.nextweb;
 
 import io.nextweb.operations.entity.EntityRequestOperations;
 
+/**
+ * <p>
+ * 
+ * @author <a href="http://www.mxro.de">Max Rohde</a>
+ *
+ */
 public interface Query extends Entity, EntityRequestOperations<Query> {
 
 }
