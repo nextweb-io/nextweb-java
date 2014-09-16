4510902839d86f21e9d649522a0abb720d253c8f
mxro <mxro@nowhere.com>
2014-09-17 08:39:03 +1000

    Updating documentation Autocommit Wed Sep 17 08:39:03 EST 2014

--- src/main/java/io/nextweb/Query.java
+++ src/main/java/io/nextweb/Query.java
@@ -4,8 +4,17 @@ import io.nextweb.operations.entity.EntityRequestOperations;
 
 /**
  * <p>
- * A Query is the possibility of a {@link Node}
+ * A Query is a special form of {@link Entity} representing the possibility of a
+ * {@link Node}.
+ * <p>
+ * Specifically, calling the {@link #get()} method on a Query should return a
+ * Node if there are no errors.
  * 
+ * 
+ * @see <a href="http://nextweb.io/docs/nextweb-entity.value.html">Entities
+ *      (Nextweb API Documentation)</a>
+ * @see <a href="http://nextweb.io/docs/nextweb-get.value.html">Get Operation
+ *      (Nextweb API Documentation)</a>
  * @author <a href="http://www.mxro.de">Max Rohde</a>
  *
  */
