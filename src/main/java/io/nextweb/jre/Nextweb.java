d61d6aa1308fef284e29de3e2dde4473a4f2f235
mxro <mxro@nowhere.com>
2014-09-19 13:38:25 +1000

    Reworking embedded server API Autocommit Fri Sep 19 13:38:25 EST 2014

--- src/main/java/io/nextweb/jre/Nextweb.java
+++ src/main/java/io/nextweb/jre/Nextweb.java
@@ -8,85 +8,82 @@ import io.nextweb.engine.NextwebGlobal;
 
 public class Nextweb {
 
-	public static void assertEngine() {
-		if (NextwebGlobal.isEngineInitialized()) {
-			return;
-		}
+    public static void assertEngine() {
+        if (NextwebGlobal.isEngineInitialized()) {
+            return;
+        }
 
-		// if not engine is initialized, try to fall back to reference engine
-		// implementations
+        // if not engine is initialized, try to fall back to reference engine
+        // implementations
 
-		try {
-			final Class<?> referenceEngine = scanClasspath(knownEngineImplementations);
-			NextwebGlobal.injectEngine((NextwebEngine) referenceEngine
-					.newInstance());
-		} catch (final Throwable e) {
-			throw new IllegalStateException("Engine could not be initialized.",
-					e);
-		}
+        try {
+            final Class<?> referenceEngine = scanClasspath(knownEngineImplementations);
+            System.out.println("here.");
+            NextwebGlobal.injectEngine((NextwebEngine) referenceEngine.newInstance());
+        } catch (final Throwable e) {
+            throw new IllegalStateException("Engine could not be initialized.", e);
+        }
 
-	}
+    }
 
-	public static void assertStartServerCapability() {
-		assertEngine();
-		try {
+    public static void assertStartServerCapability() {
+        assertEngine();
+        try {
 
-			if (NextwebGlobal.getEngine().hasStartServerCapability()) {
-				return;
-			}
+            if (NextwebGlobal.getEngine().hasStartServerCapability()) {
+                return;
+            }
 
-			// if no capability is initialized, try to fall back to reference
-			// capabilities
-			final Class<?> referenceCapability = scanClasspath(knownStartServerCapabilityImplementations);
+            // if no capability is initialized, try to fall back to reference
+            // capabilities
+            final Class<?> referenceCapability = scanClasspath(knownStartServerCapabilityImplementations);
 
-			NextwebGlobal.getEngine().injectCapability(
-					(Capability) referenceCapability.newInstance());
+            NextwebGlobal.getEngine().injectCapability((Capability) referenceCapability.newInstance());
 
-		} catch (final Throwable t) {
-			throw new IllegalStateException(
-					"Start server capability could not be instantiated.", t);
-		}
-	}
+        } catch (final Throwable t) {
+            throw new IllegalStateException("Start server capability could not be instantiated.", t);
+        }
+    }
 
-	public static LocalServer startServer(final int port) {
-		assertStartServerCapability();
+    public static LocalServer startServer(final int port) {
+        assertStartServerCapability();
 
-		return NextwebGlobal.getEngine().startServer(port);
-	}
+        return NextwebGlobal.getEngine().startServer(port);
+    }
 
-	public static Session createSession() {
-		assertEngine();
+    public static Session createSession() {
+        assertEngine();
 
-		return NextwebGlobal.getEngine().createSession();
-	}
+        return NextwebGlobal.getEngine().createSession();
+    }
 
-	public static NextwebEngine getEngine() {
-		assertEngine();
+    public static NextwebEngine getEngine() {
+        assertEngine();
 
-		return NextwebGlobal.getEngine();
-	}
+        return NextwebGlobal.getEngine();
+    }
 
-	public static void main(final String[] args) {
-		System.out.println("Please see http://nextweb.io/ for more infos.");
-	}
+    public static void main(final String[] args) {
+        System.out.println("Please see http://nextweb.io/ for more infos.");
+    }
 
-	public static String[] knownEngineImplementations = new String[] { "com.ononedb.nextweb.jre.OnedbNextwebEngineJre" };
+    public static String[] knownEngineImplementations = new String[] { "com.ononedb.nextweb.jre.OnedbNextwebEngineJre" };
 
-	public static String[] knownStartServerCapabilityImplementations = new String[] { "com.ononedb.nextweb.local.jre.OnedbStartServerCapabilityJre" };
+    public static String[] knownStartServerCapabilityImplementations = new String[] { "com.ononedb.nextweb.local.jre.OnedbStartServerCapabilityJre" };
 
-	public static Class<?> scanClasspath(final String[] knownImplementations) {
+    public static Class<?> scanClasspath(final String[] knownImplementations) {
 
-		for (final String className : knownImplementations) {
-			try {
-				final Class<?> referenceEngine = Class.forName(className);
-				return referenceEngine;
-			} catch (final ClassNotFoundException e) {
-				// nothing
-			}
-		}
+        for (final String className : knownImplementations) {
+            try {
+                final Class<?> referenceEngine = Class.forName(className);
+                return referenceEngine;
+            } catch (final ClassNotFoundException e) {
+                // nothing
+            }
+        }
 
-		throw new RuntimeException(new ClassNotFoundException(
-				"Cannot find any known Nextweb implementation on classpath."));
-	}
+        throw new RuntimeException(new ClassNotFoundException(
+                "Cannot find any known Nextweb implementation on classpath."));
+    }
 
 }
