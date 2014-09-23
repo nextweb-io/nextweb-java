0499863559c998b575940dbbff79a3c4bfb80bc6
mxro <mxro@nowhere.com>
2014-09-23 15:36:32 +1000

    Fixing bugs with new local server implementation Autocommit Tue Sep 23 15:36:32 EST 2014

--- src/main/java/io/nextweb/engine/NextwebGlobal.java
+++ src/main/java/io/nextweb/engine/NextwebGlobal.java
@@ -4,28 +4,37 @@ import io.nextweb.promise.exceptions.ExceptionManager;
 
 public class NextwebGlobal {
 
-	private static NextwebEngine definedEngine;
+    private static NextwebEngine definedEngine;
 
-	public static void injectEngine(final NextwebEngine engine) {
-		definedEngine = engine;
-		ExceptionManager.fallbackExceptionManager = engine.getExceptionManager();
-	
-	}
+    private static StartServerCapability startServerCapability;
 
-	private static NextwebEngine assertEngine() {
-		if (definedEngine == null) {
-			throw new IllegalStateException(
-					"Please initialize at least one Nextweb engine first.");
-		}
-		return definedEngine;
-	}
+    public static void injectStartServerCapability(final StartServerCapability capability) {
+        startServerCapability = capability;
+    }
 
-	public static boolean isEngineInitialized() {
-		return definedEngine != null;
-	}
+    public static StartServerCapability getStartServerCapability() {
+        return startServerCapability;
+    }
 
-	public static NextwebEngine getEngine() {
-		return assertEngine();
-	}
+    public static void injectEngine(final NextwebEngine engine) {
+        definedEngine = engine;
+        ExceptionManager.fallbackExceptionManager = engine.getExceptionManager();
+
+    }
+
+    private static NextwebEngine assertEngine() {
+        if (definedEngine == null) {
+            throw new IllegalStateException("Please initialize at least one Nextweb engine first.");
+        }
+        return definedEngine;
+    }
+
+    public static boolean isEngineInitialized() {
+        return definedEngine != null;
+    }
+
+    public static NextwebEngine getEngine() {
+        return assertEngine();
+    }
 
 }
