4d6e74bf41f920f61e874498a065c7d7ebef54f7
mxro <mxro@nowhere.com>
2014-09-23 10:05:00 +1000

    Fixing bug that same service cannot be subscribed twice Autocommit Tue Sep 23 10:05:00 EST 2014

--- src/main/java/io/nextweb/engine/StartServerCapability.java
+++ src/main/java/io/nextweb/engine/StartServerCapability.java
@@ -3,5 +3,7 @@ package io.nextweb.engine;
 import io.nextweb.common.LocalServer;
 
 public interface StartServerCapability extends Capability {
-	public LocalServer startServer(int port);
+
+    public LocalServer startServer(int port);
+
 }
