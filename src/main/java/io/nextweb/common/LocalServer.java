793c154d2eb9b9d8d44a06aa3f8568aa4095d28f
mxro <mxro@nowhere.com>
2014-10-14 09:55:33 +1100

    Fixing that servers cannot be passed to initialize sessions Autocommit Tue Oct 14 09:55:33 EST 2014

--- src/main/java/io/nextweb/common/LocalServer.java
+++ src/main/java/io/nextweb/common/LocalServer.java
@@ -6,8 +6,17 @@ import de.mxro.fn.Success;
 
 public interface LocalServer {
 
+    /**
+     * 
+     * @return A nextweb engine which is aware of this local server.
+     */
     public NextwebEngine engine();
 
+    /**
+     * Finalizes this server and releases any associated resources.
+     * 
+     * @return A promise which triggers the server shutdown.
+     */
     public NextwebPromise<Success> shutdown();
 
 }
