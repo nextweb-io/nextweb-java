9df24370cbd6e72954f7777a7978f837511cc831
mxro <mxro@nowhere.com>
2014-11-17 07:35:57 +1100

    Fixing that removeSafe operation is not executed eagerly Autocommit Mon Nov 17 07:35:57 EST 2014

--- src/main/java/io/nextweb/operations/entity/EntityRemoveOperations.java
+++ src/main/java/io/nextweb/operations/entity/EntityRemoveOperations.java
@@ -1,41 +1,46 @@
 package io.nextweb.operations.entity;
 
-import de.mxro.fn.Success;
 import io.nextweb.Entity;
 import io.nextweb.promise.NextwebPromise;
+import de.mxro.fn.Success;
 
 public interface EntityRemoveOperations {
 
-	/**
-	 * <p>
-	 * Remove a connection between nodes.
-	 * </p>
-	 * 
-	 * <p>
-	 * This operation is executed eagerly (see <a
-	 * href="http://nextweb.io/docs/nextweb-eager-operations.value.html">Lazy
-	 * and Eager operations</a>).
-	 * </p>
-	 * 
-	 * @param entity
-	 * @return
-	 */
-	public NextwebPromise<Success> remove(Entity entity);
+    /**
+     * <p>
+     * Remove a connection between nodes.
+     * </p>
+     * 
+     * <p>
+     * This operation is executed eagerly (see <a
+     * href="http://nextweb.io/docs/nextweb-eager-operations.value.html">Lazy
+     * and Eager operations</a>).
+     * </p>
+     * 
+     * @param entity
+     * @return
+     */
+    public NextwebPromise<Success> remove(Entity entity);
 
-	/**
-	 * <p>
-	 * Remove a connection between nodes.
-	 * </p>
-	 * 
-	 * <p>
-	 * This operation is executed eagerly (see <a
-	 * href="http://nextweb.io/docs/nextweb-eager-operations.value.html">Lazy
-	 * and Eager operations</a>).
-	 * </p>
-	 * 
-	 * @param entity
-	 * @return
-	 */
-	public NextwebPromise<Success> removeSafe(Entity entity);
+    /**
+     * <p>
+     * Remove a connection between nodes.
+     * </p>
+     * 
+     * <p>
+     * Note that this operation is only executed once session.commit() is
+     * called.
+     * </p>
+     * 
+     * <p>
+     * This operation is executed eagerly (see <a
+     * href="http://nextweb.io/docs/nextweb-eager-operations.value.html">Lazy
+     * and Eager operations</a>).
+     * </p>
+     * 
+     * @param entity
+     * @return
+     */
+    public NextwebPromise<Success> removeSafe(Entity entity);
 
 }
