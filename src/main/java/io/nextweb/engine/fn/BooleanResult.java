45ff1cd81e4e7b23af5ef14b4869f3d243fef704
mxro <mxro@nowhere.com>
2015-01-12 13:24:07 +1100

    Merging Promise Implementations Autocommit Mon Jan 12 13:24:07 EST 2015

--- src/main/java/io/nextweb/engine/fn/BooleanResult.java
+++ src/main/java/io/nextweb/engine/fn/BooleanResult.java
@@ -1,6 +1,5 @@
 package io.nextweb.engine.fn;
 
-import de.mxro.fn.Closure;
 import io.nextweb.Session;
 import io.nextweb.operations.callbacks.CallbackFactory;
 import io.nextweb.promise.BasicPromise;
@@ -9,148 +8,139 @@ import io.nextweb.promise.NextwebPromise;
 import io.nextweb.promise.callbacks.NextwebCallback;
 import io.nextweb.promise.exceptions.AllInterceptor;
 import io.nextweb.promise.exceptions.ExceptionListener;
-import io.nextweb.promise.exceptions.NextwebExceptionManager;
 import io.nextweb.promise.exceptions.ImpossibleListener;
+import io.nextweb.promise.exceptions.NextwebExceptionManager;
 import io.nextweb.promise.exceptions.UnauthorizedListener;
 import io.nextweb.promise.exceptions.UndefinedListener;
+import de.mxro.async.callbacks.ValueCallback;
+import de.mxro.fn.Closure;
 
-public class BooleanResult implements BasicPromise<Boolean>,
-		AllInterceptor<BooleanResult> {
+public class BooleanResult implements BasicPromise<Boolean>, AllInterceptor<BooleanResult> {
 
-	private final NextwebPromise<Boolean> result;
-	private final NextwebExceptionManager exceptionManager;
-	private final Session session;
+    private final NextwebPromise<Boolean> promise;
+    private final NextwebExceptionManager exceptionManager;
+    private final Session session;
 
-	@Override
-	public NextwebExceptionManager getExceptionManager() {
-		return exceptionManager;
-	}
+    @Override
+    public NextwebExceptionManager getExceptionManager() {
+        return exceptionManager;
+    }
 
-	public BooleanResult and(final BooleanResult otherResult) {
-		return new BooleanResult(exceptionManager, session,
-				new NextwebOperation<Boolean>() {
+    public BooleanResult and(final BooleanResult otherResult) {
+        return new BooleanResult(exceptionManager, session, new NextwebOperation<Boolean>() {
 
-					@Override
-					public void apply(final NextwebCallback<Boolean> callback) {
-						result.apply(CallbackFactory.embeddedCallback(
-								exceptionManager, callback,
-								new Closure<Boolean>() {
+            @Override
+            public void apply(final NextwebCallback<Boolean> callback) {
+                promise.apply(CallbackFactory.embeddedCallback(exceptionManager, callback, new Closure<Boolean>() {
 
-									@Override
-									public void apply(final Boolean o) {
-										if (!o.booleanValue()) {
-											callback.onSuccess(false);
-											return;
-										}
+                    @Override
+                    public void apply(final Boolean o) {
+                        if (!o.booleanValue()) {
+                            callback.onSuccess(false);
+                            return;
+                        }
 
-										otherResult.apply(CallbackFactory
-												.embeddedCallback(
-														exceptionManager,
-														callback,
-														new Closure<Boolean>() {
+                        otherResult.apply(CallbackFactory.embeddedCallback(exceptionManager, callback,
+                                new Closure<Boolean>() {
 
-															@Override
-															public void apply(
-																	final Boolean o) {
-																callback.onSuccess(o);
-															}
-														}));
+                                    @Override
+                                    public void apply(final Boolean o) {
+                                        callback.onSuccess(o);
+                                    }
+                                }));
 
-									}
+                    }
 
-								}));
-					}
-				});
-	}
+                }));
+            }
+        });
+    }
 
-	public BooleanResult or(final BooleanResult otherResult) {
-		return new BooleanResult(exceptionManager, session,
-				new NextwebOperation<Boolean>() {
+    public BooleanResult or(final BooleanResult otherResult) {
+        return new BooleanResult(exceptionManager, session, new NextwebOperation<Boolean>() {
 
-					@Override
-					public void apply(final NextwebCallback<Boolean> callback) {
-						result.apply(CallbackFactory.embeddedCallback(
-								exceptionManager, callback,
-								new Closure<Boolean>() {
+            @Override
+            public void apply(final NextwebCallback<Boolean> callback) {
+                promise.apply(CallbackFactory.embeddedCallback(exceptionManager, callback, new Closure<Boolean>() {
 
-									@Override
-									public void apply(final Boolean o) {
-										if (o.booleanValue()) {
-											callback.onSuccess(true);
-											return;
-										}
+                    @Override
+                    public void apply(final Boolean o) {
+                        if (o.booleanValue()) {
+                            callback.onSuccess(true);
+                            return;
+                        }
 
-										otherResult.apply(CallbackFactory
-												.embeddedCallback(
-														exceptionManager,
-														callback,
-														new Closure<Boolean>() {
+                        otherResult.apply(CallbackFactory.embeddedCallback(exceptionManager, callback,
+                                new Closure<Boolean>() {
 
-															@Override
-															public void apply(
-																	final Boolean o) {
-																callback.onSuccess(o);
-															}
-														}));
+                                    @Override
+                                    public void apply(final Boolean o) {
+                                        callback.onSuccess(o);
+                                    }
+                                }));
 
-									}
+                    }
 
-								}));
-					}
-				});
-	}
+                }));
+            }
+        });
+    }
 
-	@Override
-	public Boolean get() {
-		return result.get();
-	}
+    @Override
+    public Boolean get() {
+        return promise.get();
+    }
 
-	@Override
-	public void get(final Closure<Boolean> callback) {
-		result.get(callback);
-	}
+    @Override
+    public void get(final Closure<Boolean> callback) {
+        promise.get(callback);
+    }
 
-	@Override
-	public void apply(final NextwebCallback<Boolean> callback) {
-		result.apply(callback);
-	}
+    @Override
+    public void apply(final NextwebCallback<Boolean> callback) {
+        promise.apply(callback);
+    }
 
-	@Override
-	public BooleanResult catchUnauthorized(final UnauthorizedListener listener) {
-		exceptionManager.catchUnauthorized(listener);
-		return this;
-	}
+    @Override
+    public BooleanResult catchUnauthorized(final UnauthorizedListener listener) {
+        exceptionManager.catchUnauthorized(listener);
+        return this;
+    }
 
-	@Override
-	public BooleanResult catchImpossible(final ImpossibleListener listener) {
-		exceptionManager.catchImpossible(listener);
-		return this;
-	}
+    @Override
+    public BooleanResult catchImpossible(final ImpossibleListener listener) {
+        exceptionManager.catchImpossible(listener);
+        return this;
+    }
 
-	@Override
-	public BooleanResult catchExceptions(final ExceptionListener listener) {
-		exceptionManager.catchExceptions(listener);
-		return this;
-	}
+    @Override
+    public BooleanResult catchExceptions(final ExceptionListener listener) {
+        exceptionManager.catchExceptions(listener);
+        return this;
+    }
 
-	@Override
-	public BooleanResult catchUndefined(final UndefinedListener listener) {
-		exceptionManager.catchUndefined(listener);
-		return this;
-	}
+    @Override
+    public BooleanResult catchUndefined(final UndefinedListener listener) {
+        exceptionManager.catchUndefined(listener);
+        return this;
+    }
 
-	@Override
-	public NextwebOperation<Boolean> getOriginalOperation() {
-		return this.result;
-	}
+    @Override
+    public NextwebOperation<Boolean> getOriginalOperation() {
+        return this.promise;
+    }
 
-	public BooleanResult(final NextwebExceptionManager eM, final Session session,
-			final NextwebOperation<Boolean> result) {
-		super();
-		this.exceptionManager = new NextwebExceptionManager(eM);
-		this.session = session;
-		this.result = session.getEngine().getFactory()
-				.createPromise(this.exceptionManager, session, result);
+    public BooleanResult(final NextwebExceptionManager eM, final Session session, final NextwebOperation<Boolean> result) {
+        super();
+        this.exceptionManager = new NextwebExceptionManager(eM);
+        this.session = session;
+        this.promise = session.getEngine().getFactory().createPromise(this.exceptionManager, session, result);
 
-	}
+    }
+
+    @Override
+    public void apply(final ValueCallback<Boolean> callback) {
+        this.promise.apply(callback);
+
+    }
 }
--- src/main/java/io/nextweb/engine/fn/IntegerResult.java
+++ src/main/java/io/nextweb/engine/fn/IntegerResult.java
@@ -1,6 +1,5 @@
 package io.nextweb.engine.fn;
 
-import de.mxro.fn.Closure;
 import io.nextweb.Session;
 import io.nextweb.operations.callbacks.CallbackFactory;
 import io.nextweb.promise.BasicPromise;
@@ -9,144 +8,131 @@ import io.nextweb.promise.NextwebPromise;
 import io.nextweb.promise.callbacks.NextwebCallback;
 import io.nextweb.promise.exceptions.AllInterceptor;
 import io.nextweb.promise.exceptions.ExceptionListener;
-import io.nextweb.promise.exceptions.NextwebExceptionManager;
 import io.nextweb.promise.exceptions.ImpossibleListener;
+import io.nextweb.promise.exceptions.NextwebExceptionManager;
 import io.nextweb.promise.exceptions.UnauthorizedListener;
 import io.nextweb.promise.exceptions.UndefinedListener;
+import de.mxro.async.callbacks.ValueCallback;
+import de.mxro.fn.Closure;
 
-public class IntegerResult implements BasicPromise<Integer>,
-		AllInterceptor<IntegerResult> {
+public class IntegerResult implements BasicPromise<Integer>, AllInterceptor<IntegerResult> {
 
-	private final NextwebPromise<Integer> result;
-	private final NextwebExceptionManager exceptionManager;
-	private final Session session;
+    private final NextwebPromise<Integer> promise;
+    private final NextwebExceptionManager exceptionManager;
+    private final Session session;
 
-	public IntegerResult plus(final IntegerResult otherResult) {
-		return new IntegerResult(exceptionManager, session,
-				new NextwebOperation<Integer>() {
+    public IntegerResult plus(final IntegerResult otherResult) {
+        return new IntegerResult(exceptionManager, session, new NextwebOperation<Integer>() {
 
-					@Override
-					public void apply(final NextwebCallback<Integer> callback) {
+            @Override
+            public void apply(final NextwebCallback<Integer> callback) {
 
-						result.apply(CallbackFactory.embeddedCallback(
-								exceptionManager, callback,
-								new Closure<Integer>() {
+                promise.apply(CallbackFactory.embeddedCallback(exceptionManager, callback, new Closure<Integer>() {
 
-									@Override
-									public void apply(
-											final Integer thisResultValue) {
+                    @Override
+                    public void apply(final Integer thisResultValue) {
 
-										otherResult.apply(CallbackFactory
-												.embeddedCallback(
-														exceptionManager,
-														callback,
-														new Closure<Integer>() {
+                        otherResult.apply(CallbackFactory.embeddedCallback(exceptionManager, callback,
+                                new Closure<Integer>() {
 
-															@Override
-															public void apply(
-																	final Integer otherResultValue) {
-																callback.onSuccess(thisResultValue
-																		+ otherResultValue);
-															}
-														}));
+                                    @Override
+                                    public void apply(final Integer otherResultValue) {
+                                        callback.onSuccess(thisResultValue + otherResultValue);
+                                    }
+                                }));
 
-									}
-								}));
-					}
-				});
-	}
+                    }
+                }));
+            }
+        });
+    }
 
-	public IntegerResult minus(final IntegerResult otherResult) {
-		return new IntegerResult(exceptionManager, session,
-				new NextwebOperation<Integer>() {
+    public IntegerResult minus(final IntegerResult otherResult) {
+        return new IntegerResult(exceptionManager, session, new NextwebOperation<Integer>() {
 
-					@Override
-					public void apply(final NextwebCallback<Integer> callback) {
+            @Override
+            public void apply(final NextwebCallback<Integer> callback) {
 
-						result.apply(CallbackFactory.embeddedCallback(
-								exceptionManager, callback,
-								new Closure<Integer>() {
+                promise.apply(CallbackFactory.embeddedCallback(exceptionManager, callback, new Closure<Integer>() {
 
-									@Override
-									public void apply(
-											final Integer thisResultValue) {
+                    @Override
+                    public void apply(final Integer thisResultValue) {
 
-										otherResult.apply(CallbackFactory
-												.embeddedCallback(
-														exceptionManager,
-														callback,
-														new Closure<Integer>() {
+                        otherResult.apply(CallbackFactory.embeddedCallback(exceptionManager, callback,
+                                new Closure<Integer>() {
 
-															@Override
-															public void apply(
-																	final Integer otherResultValue) {
-																callback.onSuccess(thisResultValue
-																		- otherResultValue);
-															}
-														}));
+                                    @Override
+                                    public void apply(final Integer otherResultValue) {
+                                        callback.onSuccess(thisResultValue - otherResultValue);
+                                    }
+                                }));
 
-									}
-								}));
-					}
-				});
-	}
+                    }
+                }));
+            }
+        });
+    }
 
-	@Override
-	public Integer get() {
-		return result.get();
-	}
+    @Override
+    public Integer get() {
+        return promise.get();
+    }
 
-	@Override
-	public void get(final Closure<Integer> callback) {
-		result.get(callback);
-	}
+    @Override
+    public void get(final Closure<Integer> callback) {
+        promise.get(callback);
+    }
 
-	@Override
-	public void apply(final NextwebCallback<Integer> callback) {
-		result.apply(callback);
-	}
+    @Override
+    public void apply(final NextwebCallback<Integer> callback) {
+        promise.apply(callback);
+    }
 
-	@Override
-	public NextwebExceptionManager getExceptionManager() {
-		return exceptionManager;
-	}
+    @Override
+    public NextwebExceptionManager getExceptionManager() {
+        return exceptionManager;
+    }
 
-	@Override
-	public IntegerResult catchUndefined(final UndefinedListener listener) {
-		exceptionManager.catchUndefined(listener);
-		return this;
-	}
+    @Override
+    public IntegerResult catchUndefined(final UndefinedListener listener) {
+        exceptionManager.catchUndefined(listener);
+        return this;
+    }
 
-	@Override
-	public IntegerResult catchUnauthorized(final UnauthorizedListener listener) {
-		exceptionManager.catchUnauthorized(listener);
-		return this;
-	}
+    @Override
+    public IntegerResult catchUnauthorized(final UnauthorizedListener listener) {
+        exceptionManager.catchUnauthorized(listener);
+        return this;
+    }
 
-	@Override
-	public IntegerResult catchExceptions(final ExceptionListener listener) {
-		exceptionManager.catchExceptions(listener);
-		return this;
-	}
+    @Override
+    public IntegerResult catchExceptions(final ExceptionListener listener) {
+        exceptionManager.catchExceptions(listener);
+        return this;
+    }
 
-	@Override
-	public IntegerResult catchImpossible(final ImpossibleListener listener) {
-		exceptionManager.catchImpossible(listener);
-		return this;
-	}
+    @Override
+    public IntegerResult catchImpossible(final ImpossibleListener listener) {
+        exceptionManager.catchImpossible(listener);
+        return this;
+    }
 
-	public IntegerResult(final NextwebExceptionManager parentExceptionManager,
-			final Session session, final NextwebOperation<Integer> result) {
-		super();
-		this.exceptionManager = new NextwebExceptionManager(parentExceptionManager);
-		this.session = session;
-		this.result = session.getEngine().getFactory()
-				.createPromise(this.exceptionManager, session, result);
-	}
+    public IntegerResult(final NextwebExceptionManager parentExceptionManager, final Session session,
+            final NextwebOperation<Integer> result) {
+        super();
+        this.exceptionManager = new NextwebExceptionManager(parentExceptionManager);
+        this.session = session;
+        this.promise = session.getEngine().getFactory().createPromise(this.exceptionManager, session, result);
+    }
 
-	@Override
-	public NextwebOperation<Integer> getOriginalOperation() {
-		return this.result;
-	}
+    @Override
+    public NextwebOperation<Integer> getOriginalOperation() {
+        return this.promise;
+    }
+
+    @Override
+    public void apply(final ValueCallback<Integer> callback) {
+        this.promise.apply(callback);
+    }
 
 }
